a0af3587b357e32e8e4399a34329fa71c90ffed7
poolieweb <poolieweb@yahoo.co.uk>
2018-11-06 11:21:26 +0000

    First group of examples

---
+++ Sessions/GoLang-100/101/1-HelloWorld/hello-world.go
@@ -0,0 +1,17 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println("hello world")
+}
+
+// $ go run hello-world.go
+// hello world
+
+// $ go build hello-world.go
+// $ ls
+// hello-world	hello-world.go
+
+// $ ./hello-world
+// hello world
