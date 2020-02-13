f7f25ca7e9a296cc8c7d2f1a7458335dad3c9f5e
DJWaddsIW <daniel.waddington@infinityworks.com>
2020-02-13 10:40:01 +0000

    Converted const to functions in order to add converting functions and prop deconstruction to React-101.002

--- sessions/React-100/101/101.002/index.js
+++ sessions/React-100/101/101.002/index.js
@@ -1,24 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-class Hello extends React.Component {
-    render() {
-      return (
-        <div>
-          Hello {this.props.name}
-        </div>
-      );
-    }
-  }
-
-// As a stateless functional component.
-const HelloWorld = ({name}) => (
+// Convert to ES6 const
+function HelloWorld (props) {
+  return (
     <div>
-        Hello {name}
+      Hello {props.name}
     </div>
-);
+  )
+}
 
-ReactDOM.render(<Hello name="Mr Anderson" />, document.getElementById('app'));
+ReactDOM.render(<HelloWorld name="Mr Anderson" />, document.getElementById('app'));
 
 if (module.hot) {
     module.hot.accept();
