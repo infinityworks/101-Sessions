7c4cb4f7a9463b557e50e99ae16bddc6f84d71e1
Ben Foster <ben.foster@infinityworks.com>
2020-06-02 08:52:34 +0100

    Added solution for react router navigation lesson

---
+++ sessions/React-100/101/101.008-solution/index.js
@@ -0,0 +1,50 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
+import Home from './pages/Home';
+import About from "./pages/About";
+import Contact from "./pages/Contact";
+
+
+const App = () => (
+  <div>
+    <h1>Site Header</h1>
+    <Router>
+      <nav>
+        <ul>
+          <li>
+            <Link to="/">Home</Link>
+          </li>
+          <li>
+            <Link to="/about">About</Link>
+          </li>
+          <li>
+            <Link to="/contact">Contact</Link>
+          </li>
+        </ul>
+      </nav>
+      <Switch>
+        <Route path="/about">
+          <About />
+        </Route>
+        <Route path="/contact">
+          <Contact />
+        </Route>
+        <Route path="/">
+          <Home />
+        </Route>
+      </Switch>
+    </Router>
+  </div>
+);
+
+ReactDOM.render(<App />, document.getElementById('app'));
+
+if (module.hot) {
+    module.hot.accept();
+}
---
+++ sessions/React-100/101/101.008-solution/pages/Home/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Home = () => (
+  <div>
+    <h1>Home</h1>
+    <div>
+      <p>Test Home text</p>
+    </div>
+  </div>
+);
+
+export default Home;
