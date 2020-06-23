385ce7f21da059cf24cb19710b0deeaa5b13f31b
Robert Rosebury <robert.rosebury@infinityworks.com>
2020-06-23 20:38:01 +0100

    [Redux] Add redux-logger middleware
        - Added redux-logger middleware, see store.js state changes are now output to the browser’s console

--- sessions/React-100/101/102.002/store.js
+++ sessions/React-100/101/102.002/store.js
@@ -1,8 +1,5 @@
-import { createStore } from "redux";
+import { createStore, applyMiddleware } from "redux";
 import rootReducer from "./reducers";
+import logger from "redux-logger";
 
-export default createStore(
-  rootReducer,
-  {},
-  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
-);
+export default createStore(rootReducer, {}, applyMiddleware(logger));
