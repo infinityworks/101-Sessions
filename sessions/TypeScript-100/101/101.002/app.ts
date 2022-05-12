9471439d2f368283782a15acc1f68e02073bf474
EhsanKh <ehsankhfr@gmail.com>
2022-05-12 17:29:17 +0100

    fix: code formatting

--- sessions/TypeScript-100/101/101.002/app.ts
+++ sessions/TypeScript-100/101/101.002/app.ts
@@ -4,13 +4,14 @@
 function halve(x: number) {
     console.log(x / 2);
 }
+
 halve(10); // 5.0
 halve("some string"); // compile error: Argument of type 'string' is not assignable to parameter of type 'number'.
 
 /*
  A bit more complicated
 */
-let obj = {
+const obj = {
     name: "Test Object",
     age: 42
 };
@@ -21,31 +22,40 @@ console.log(obj.size); // compile error: Property 'size' does not exist on type 
 /*
  A tiny bit more complicated again
 */
-function printSize(obj: {size: number}) {
+function printSize(obj: { size: number }) {
     console.log(obj.size);
 }
 
-let x = { size: 10 };
+const x = {size: 10};
 printSize(x); // 10
 
-let y = { size: "10" };
+const y = {size: "10"};
 printSize(y); // compile error: Type 'string' is not assignable to type 'number'.
 
-let z = { age: 42 };
+const z = {age: 42};
 printSize(z); // compile error: Property 'size' is missing in type '{ age: number; }' but required in type '{ size: number; }'.
 
 
+/*
+    Defines a type of object which has a `sayHello` function which
+    accepts a string and returns a string.
+*/
+type CanSayHello = {
+    sayHello(name: string): string
+}
+
 function callSayHello(
-    obj: {sayHello(name: string): string}
+    obj: CanSayHello
 ) {
-    let name: string = "world";
-    let greeting: string = obj.sayHello(name); // call function on the object
+    const name: string = "world";
+    const greeting: string = obj.sayHello(name); // call function on the object
     console.log(greeting);
 }
 
-callSayHello({
-    // object knows how to say hello
+const csh: CanSayHello = {
     sayHello(name: string): string {
         return `hello ${name}`;
     }
-});
+};
+callSayHello(csh);
+
