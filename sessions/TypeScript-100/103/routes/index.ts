d74a82a2d343a53f094a697d5af33ccefd68fd0e
Tom Moreton <tom.moreton@live.co.uk>
2022-05-18 16:22:09 +0100

    Semicolons

--- sessions/TypeScript-100/103/routes/index.ts
+++ sessions/TypeScript-100/103/routes/index.ts
@@ -1,4 +1,4 @@
-import { NextFunction, Request, Response, Router } from 'express'
+import { NextFunction, Request, Response, Router } from 'express';
 const router = Router();
 
 /* GET home page. */
