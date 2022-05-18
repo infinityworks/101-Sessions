5f2b80aad34b22905f6f7b70ce19d1f1b94549be
Tom Moreton <tom.moreton@live.co.uk>
2022-05-18 16:08:31 +0100

    Use more detailed logging in production

--- sessions/TypeScript-100/103/app.ts
+++ sessions/TypeScript-100/103/app.ts
@@ -1,14 +1,14 @@
 import express, { Application } from 'express';
 import path from 'path';
 import cookieParser from 'cookie-parser';
-import morgan from 'morgan';
+import logger from 'morgan';
 
 import indexRouter from './routes/index';
 import usersRouter from './routes/users';
 
 const app: Application = express();
 
-app.use(morgan('dev'));
+app.use(process.env.NODE_ENV === 'development' ? logger('dev') : logger('combined'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
