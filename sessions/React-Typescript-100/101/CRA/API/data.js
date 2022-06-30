d92b3a256211def12ff08f57f1fb27fa4e169b71
Tauseef Iqbal <tauseef494@gmail.com>
2022-06-30 11:05:36 +0100

    data.js returns mock json data response

--- sessions/React-Typescript-100/101/CRA/API/data.js
+++ sessions/React-Typescript-100/101/CRA/API/data.js
@@ -1,44 +1,54 @@
-const getLogin = (req, res) => {
-    const username = req.body.userName || 'Cherise';
-    const password = req.body.password;
-      
-    const response = res.status(200).send({ message: 'React 101 login', userId: username });
+const data = require('./data.json');
 
-    return response;
-}
+const getRoute = (req, res) => {
+  const response = res.status(200).send({ message: 'Success', data: data });
+  return response;
+};
+
+const getLogin = (req, res) => {
+  const username = req.body.userName || 'No username';
+  const password = req.body.password || 'No Password';
+
+  const response = res
+    .status(200)
+    .send({ message: 'Success', userId: data.userId });
+
+  return response;
+};
 
 const addPostcode = (req, res) => {
-    const userId = req.body.userId || 'No ID provided';
-    const postcode = req.body.postcode || 'No postcode';
+  const userId = req.body.userId || 'No ID provided';
+  const postcode = req.body.postcode || 'No postcode';
 
-    const response = res.status(200).send({message: 'Post Code Added'});
+  const response = res.status(200).send({ message: 'Post Code Added' });
 
-    return response;
-}
+  return response;
+};
 
 const deletePostcode = (req, res) => {
-    const userId = req.body.userId || 'No ID provided';
-    const postcode = req.body.postcode || 'No postcode';
-    
-    const response = res.status(200).send({message: 'Post Code Deleted'});
-    
-    return response;
-}
+  const userId = req.body.userId || 'No ID provided';
+  const postcode = req.body.postcode || 'No postcode';
+
+  const response = res.status(200).send({ message: 'Post Code Deleted' });
+
+  return response;
+};
 
 const createUser = (req, res) => {
-    const username = req.body.userName || 'Cherise';
-    const password = req.body.password;
+  const username = req.body.userName || 'No username';
+  const password = req.body.password || 'No password';
 
-    const userId = req.body.userId || 'No ID provided';
+  const response = res
+    .status(200)
+    .send({ message: 'User Created', userId: data.userId });
 
-    const response = res.status(200).send({ message: 'React 101 login', userId: userId });
+  return response;
+};
 
-    return response;
-}
-
-module.exports = { 
-    getLogin, 
-    addPostcode,
-    deletePostcode, 
-    createUser
+module.exports = {
+  getRoute,
+  getLogin,
+  addPostcode,
+  deletePostcode,
+  createUser,
 };
