ae1048c0d9ef3c567a091fc5fa3f15d8f1125b1f
Balaji Lakshmanan <102376624+balalaksIW@users.noreply.github.com>
2023-03-16 11:29:41 +0000

    Update 3_Data_Loading_Int_Named_Stage.sql

--- sessions/Snowflake-100/Snowflake_Workshop/Data_Loading/Scripts/3_Data_Loading_Int_Named_Stage.sql
+++ sessions/Snowflake-100/Snowflake_Workshop/Data_Loading/Scripts/3_Data_Loading_Int_Named_Stage.sql
@@ -8,6 +8,13 @@ SHOW STAGES;
 --List the contents of internal stage
 LIST @MY_INTERNAL_STAGE;
 
+--Creating the tables
+
+CREATE TABLE REGIONS (
+  REGION_ID NUMBER(10,0)
+  , REGION_NAME VARCHAR(64)
+);
+
 --Place the file from your terminal using "put file://Documents/GitHub/101-Sessions/sessions/Snowflake-100/Snowflake_Workshop/Data_Loading/files/regions.csv @MY_INTERNAL_STAGE;"
 --Copy into the tables
 COPY INTO REGIONS FROM @MY_INTERNAL_STAGE/regions.csv.gz
