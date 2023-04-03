95853c541f07950c269571af138c45789688eb6c
arlindo.andrade <arlindo.andrade@infinityworks.com>
2023-04-03 14:50:40 +0100

    Format SQL using SQL Formatter vscode extension

--- sessions/DBT-100/DBT_Workshop/00-snowflake-setup/1_jaffle_shop_setup.sql
+++ sessions/DBT-100/DBT_Workshop/00-snowflake-setup/1_jaffle_shop_setup.sql
@@ -1,12 +1,16 @@
 use role sysadmin;
+
 use warehouse load_wh;
 
 create database dbt_workshop_raw;
+
 create schema dbt_workshop_raw.jaffle_shop;
+
 create schema dbt_workshop_raw.stripe;
 
 create database dbt_workshop_analytics;
 
+-- Create and populate orders table
 create table dbt_workshop_raw.jaffle_shop.customers (
     id integer,
     first_name varchar,
@@ -14,15 +18,12 @@ create table dbt_workshop_raw.jaffle_shop.customers (
 );
 
 copy into dbt_workshop_raw.jaffle_shop.customers (id, first_name, last_name)
-from 's3://dbt-tutorial-public/jaffle_shop_customers.csv'
-file_format = (
-    type = 'CSV'
-    field_delimiter = ','
-    skip_header = 1
-    ); 
+from
+    's3://dbt-tutorial-public/jaffle_shop_customers.csv' file_format = (
+        type = 'CSV' field_delimiter = ',' skip_header = 1
+    );
 
 -- Create and populate orders table
-
 create table dbt_workshop_raw.jaffle_shop.orders(
     id integer,
     user_id integer,
@@ -32,11 +33,9 @@ create table dbt_workshop_raw.jaffle_shop.orders(
 );
 
 copy into dbt_workshop_raw.jaffle_shop.orders (id, user_id, order_date, status)
-from 's3://dbt-tutorial-public/jaffle_shop_orders.csv'
-file_format = (
-    type = 'CSV'
-    field_delimiter = ','
-    skip_header = 1
+from
+    's3://dbt-tutorial-public/jaffle_shop_orders.csv' file_format = (
+        type = 'CSV' field_delimiter = ',' skip_header = 1
     );
 
 -- Create and populate payment table
@@ -50,10 +49,15 @@ create table dbt_workshop_raw.stripe.payment (
     _batched_at timestamp default current_timestamp
 );
 
-copy into dbt_workshop_raw.stripe.payment (id, orderid, paymentmethod, status, amount, created)
-from 's3://dbt-tutorial-public/stripe_payments.csv'
-file_format = (
-    type = 'CSV'
-    field_delimiter = ','
-    skip_header = 1
+copy into dbt_workshop_raw.stripe.payment (
+    id,
+    orderid,
+    paymentmethod,
+    status,
+    amount,
+    created
+)
+from
+    's3://dbt-tutorial-public/stripe_payments.csv' file_format = (
+        type = 'CSV' field_delimiter = ',' skip_header = 1
     );
