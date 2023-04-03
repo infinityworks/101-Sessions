d4a1d2625fe4b16d4d37aaa596e3b249879c704e
arlindo.andrade <arlindo.andrade@infinityworks.com>
2023-04-03 14:27:46 +0100

    Setup jaffle shop schema

---
+++ sessions/DBT-100/DBT_Workshop/00-snowflake-setup/1_jaffle_shop_setup.sql
@@ -0,0 +1,59 @@
+use role sysadmin;
+use warehouse load_wh;
+
+create database dbt_workshop_raw;
+create schema dbt_workshop_raw.jaffle_shop;
+create schema dbt_workshop_raw.stripe;
+
+create database dbt_workshop_analytics;
+
+create table dbt_workshop_raw.jaffle_shop.customers (
+    id integer,
+    first_name varchar,
+    last_name varchar
+);
+
+copy into dbt_workshop_raw.jaffle_shop.customers (id, first_name, last_name)
+from 's3://dbt-tutorial-public/jaffle_shop_customers.csv'
+file_format = (
+    type = 'CSV'
+    field_delimiter = ','
+    skip_header = 1
+    ); 
+
+-- Create and populate orders table
+
+create table dbt_workshop_raw.jaffle_shop.orders(
+    id integer,
+    user_id integer,
+    order_date date,
+    status varchar,
+    _etl_loaded_at timestamp default current_timestamp
+);
+
+copy into dbt_workshop_raw.jaffle_shop.orders (id, user_id, order_date, status)
+from 's3://dbt-tutorial-public/jaffle_shop_orders.csv'
+file_format = (
+    type = 'CSV'
+    field_delimiter = ','
+    skip_header = 1
+    );
+
+-- Create and populate payment table
+create table dbt_workshop_raw.stripe.payment (
+    id integer,
+    orderid integer,
+    paymentmethod varchar,
+    status varchar,
+    amount integer,
+    created date,
+    _batched_at timestamp default current_timestamp
+);
+
+copy into dbt_workshop_raw.stripe.payment (id, orderid, paymentmethod, status, amount, created)
+from 's3://dbt-tutorial-public/stripe_payments.csv'
+file_format = (
+    type = 'CSV'
+    field_delimiter = ','
+    skip_header = 1
+    );
